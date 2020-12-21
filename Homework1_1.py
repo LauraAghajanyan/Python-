3d011b40dbde052b9d69c7903473cc4c06a9d048
LauraAghajanyan <laura.aghajanyan2230@gmail.com>
2020-12-22 02:38:26 +0400

    Homework1

---
+++ Homework1_1.py
@@ -0,0 +1,5 @@
+a1 = float(input("Enter the first number: "))   # input the first number of the progression
+a2 = float(input("Enter the second number: "))  # input the second number of the progression
+d = a2-a1
+n = float(input("Enter the index: "))  # n is the index of a certain member of the progression
+print(a1+(n-1)*d)
